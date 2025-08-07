da2ef06e9acf268d622e5d1f628e317c7de12623
V3DxNT <ved4geeks@gmail.com>
2025-08-07 18:57:46 +0530

    Lecture-35: Properties and Dot Notation on Objects

---
+++ Exerscise-8-Objects/Lecture-35-DreamCar.js
@@ -0,0 +1,14 @@
+// Write code below 💖
+const car={
+    model:"Tesla",
+    year:2025,
+    color:"Black",
+    used:false
+};
+
+if(car.used === false){
+    console.log("I am looking for a "+ car.model +" that is new");
+}
+else{
+    console.log("I am looking for a "+car.model+" that is used");
+}
