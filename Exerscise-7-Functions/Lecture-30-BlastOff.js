050e971734aedbef75cc8e61f0c1cfeb764887a1
V3DxNT <ved4geeks@gmail.com>
2025-08-07 17:21:49 +0530

    Lecture-30: Return Keywords

---
+++ Exerscise-7-Functions/Lecture-30-BlastOff.js
@@ -0,0 +1,14 @@
+// Write code below 💖
+function countDown(){
+    for(let i=10;i>0;i--)
+    {
+        console.log(i);
+    }
+    return "BlastOff!";
+
+}
+
+
+countDown();                      //Prints the Values in Loop but does not account for the return
+
+console.log(countDown());     //Prints the Values in the loop as well as holds the return values
