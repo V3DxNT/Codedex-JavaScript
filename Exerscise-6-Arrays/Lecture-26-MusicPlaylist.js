c42764e4baf2e08caf86f0bd2f669209b2150e19
V3DxNT <ved4geeks@gmail.com>
2025-08-07 14:44:37 +0530

    Lecture-26: Adding and Removing Elements in Array

---
+++ Exerscise-6-Arrays/Lecture-26-MusicPlaylist.js
@@ -0,0 +1,20 @@
+// Write code below 💖
+const musicPlaylist = [
+    "Tom Sawyer",
+    "Sabotage",
+    "I Wanna Dance With Somebody",
+    "Don't Speak",
+    "Bulls On Parade",
+    "Thriller",
+    "The Breaks",
+    "Brick",
+    "Aeroplane Over the Sea",
+    "Tubthumping"
+];
+
+musicPlaylist.shift();
+musicPlaylist.pop();
+musicPlaylist.push("Tera Hua");
+musicPlaylist.unshift("Raat Bhar","Hone Laga Hu");
+
+console.log(musicPlaylist)
