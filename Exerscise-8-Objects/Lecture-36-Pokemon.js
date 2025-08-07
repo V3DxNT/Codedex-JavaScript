de0673c9d6aec4f3f2513d7bc6d751d83af72ebb
V3DxNT <ved4geeks@gmail.com>
2025-08-07 19:14:13 +0530

    Lecture-38:Updating and Adding Properties

---
+++ Exerscise-8-Objects/Lecture-36-Pokemon.js
@@ -0,0 +1,15 @@
+// Write code below 💖
+let pokemon = {
+    name: "Pikachu",
+    type: "Electric ⚡️",
+    level: 25,
+};
+
+pokemon.isCaught=false;
+console.log(pokemon);
+
+pokemon.isCaught=true;
+pokemon.name="Raichu";
+
+console.log(pokemon);
+
