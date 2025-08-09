b889665ecc472d3261e3ec47b53edb764b288b71
V3DxNT <ved4geeks@gmail.com>
2025-08-09 22:38:32 +0530

    Lecture-40-HotBling

---
+++ Exerscise-9-Trifecta/Lecture-41-HotBling/script.js
@@ -0,0 +1,7 @@
+// Write code below 💖
+let drakePicTwo=document.querySelector("#drake-pic-2")
+
+let heading= document.querySelector("#heading-2");
+drakePicTwo.src="https://i.imgur.com/RGbh6zY.png";
+
+heading.innerText="Learn By Doing";
